0d3c727472a2ab9ec59bafb91a80fb4d644b17ef
oswtimx <oswtinetwork@gmail.com>
2025-02-09 23:42:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
 // read custom message from query strings
 // Tutorial -> https://youtu.be/6ojp1iWUKw8
 
-const urlSearchParams = new URLSearchParams(window.location.search)
+const urlSearchParams = new URLSearchParams(window.location.search);
 
-const messageCustom = urlSearchParams.get('message')
+const messageCustom = urlSearchParams.get('message');
 
 if (messageCustom) {
-
-  const mainMessageElement = document.querySelector('#mainMessage')
-  mainMessageElement.textContent = decodeURI(messageCustom)
+  const mainMessageElement = document.querySelector('#mainMessage');
+  mainMessageElement.textContent = decodeURI(messageCustom);
 }
 
 // the tutorial starts here
 
-const btnOpenElement = document.querySelector('#open')
-const btnCloseElement = document.querySelector('#close')
+const btnOpenElement = document.querySelector('#open');
+const btnCloseElement = document.querySelector('#close');
 
-btnCloseElement.disabled = true
+btnCloseElement.disabled = true;
 
+btnOpenElement.addEventListener('click', () => {
+  btnOpenElement.disabled = true;
+  btnCloseElement.disabled = false;
+  const coverElement = document.querySelector('.cover');
+  coverElement.classList.add('open-cover');
 
+  setTimeout(() => {
+    coverElement.style.zIndex = -1;
 
-btnOpenElement.addEventListener('click', ()=> {
-  btnOpenElement.disabled = true
-  btnCloseElement.disabled = false
-  const coverElement = document.querySelector('.cover')
-  coverElement.classList.add('open-cover')
+    const paperElement = document.querySelector('.paper');
+    paperElement.classList.remove('close-paper');
+    paperElement.classList.add('open-paper');
 
-  setTimeout(()=>{
-    //
-    coverElement.style.zIndex = -1
-    
-    const paperElement = document.querySelector('.paper')
-    paperElement.classList.remove('close-paper')
-    paperElement.classList.add('open-paper')
+    // animacion de la paloma
+    const palomaElement = document.querySelector('.paloma');
+    palomaElement.style.display = 'block';
+  }, 500);
+});
 
-    // animacion del corazón
-    const heartElement = document.querySelector('.heart')
-    heartElement.style.display = 'block'
-  
-  }, 500)
+btnCloseElement.addEventListener('click', () => {
+  btnOpenElement.disabled = false;
+  btnCloseElement.disabled = true;
 
-})
-btnCloseElement.addEventListener('click', ()=> {
-  btnOpenElement.disabled = false
-  btnCloseElement.disabled = true
+  const coverElement = document.querySelector('.cover');
+  const paperElement = document.querySelector('.paper');
+  paperElement.classList.remove('open-paper');
+  paperElement.classList.add('close-paper');
 
-  const coverElement = document.querySelector('.cover')
-  const paperElement = document.querySelector('.paper')
-  paperElement.classList.remove('open-paper')
-  paperElement.classList.add('close-paper')
-  
-  setTimeout(()=>{
-    coverElement.style.zIndex = 0
-    coverElement.classList.remove('open-cover')
+  setTimeout(() => {
+    coverElement.style.zIndex = 0;
+    coverElement.classList.remove('open-cover');
 
-    // animacion del corazón
-    const heartElement = document.querySelector('.heart')
-    heartElement.style.display = 'none'
-  },500)
-})
+    // animacion de la paloma
+    const palomaElement = document.querySelector('.paloma');
+    palomaElement.style.display = 'none';
+  }, 500);
+});
