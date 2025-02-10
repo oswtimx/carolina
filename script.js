640db8e572bdf99e7732f70823be46490be30180
oswtimx <oswtinetwork@gmail.com>
2025-02-10 17:45:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,46 +7,7 @@ const messageCustom = urlSearchParams.get('message');
 
 if (messageCustom) {
   const mainMessageElement = document.querySelector('#mainMessage');
-  mainMessageElement.textContent = decodeURI(messageCustom);
-}
 
-// the tutorial starts here
-
-const btnOpenElement = document.querySelector('#open');
-const btnCloseElement = document.querySelector('#close');
-
-btnCloseElement.disabled = true;
-
-btnOpenElement.addEventListener('click', () => {
-  btnOpenElement.disabled = true;
-  btnCloseElement.disabled = false;
-  const coverElement = document.querySelector('.cover');
-  coverElement.classList.add('open-cover');
-
-  setTimeout(() => {
-    coverElement.style.zIndex = -1;
-
-    const paperElement = document.querySelector('.paper');
-    paperElement.classList.remove('close-paper');
-    paperElement.classList.add('open-paper');
-
-    // animacion de la paloma
-    const palomaElement = document.querySelector('.paloma');
-    palomaElement.style.display = 'block';
-  }, 500);
-});
-
-btnCloseElement.addEventListener('click', () => {
-  btnOpenElement.disabled = false;
-  btnCloseElement.disabled = true;
-
-  const coverElement = document.querySelector('.cover');
-  const paperElement = document.querySelector('.paper');
-  paperElement.classList.remove('open-paper');
-  paperElement.classList.add('close-paper');
-
-  setTimeout(() => {
-    coverElement.style.zIndex = 0;
     coverElement.classList.remove('open-cover');
 
     // animacion de la paloma
